8bac273bf77075681f642c9de6ceda50e125c483
sorihan0130 <sorihan0130@gmail.com>
2026-01-16 21:20:10 +0900

    Add 3 Person, Address, Class

---
+++ Assignment1_HanHyeYoung/src/Model/Person.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Hyungs
+ */
+public class Person {
+    String firstName;
+    String lastName;
+    String age;
+    String maritalStatus;
+    Address homeAddress;
+    Address localAddress;
+    BankAccount bankAccount;
+}
